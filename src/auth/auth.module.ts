915c6cf631d7baa526d983c4b8d293f6a95e6572
SupersantyZ <santiagojoeljesus@gmail.com>
2025-03-05 13:11:12 -0300

    token 24H

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -14,7 +14,7 @@ import { AuthController } from './auth/auth.controller';
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '1h' }, // Ajusta el tiempo de expiración según tus necesidades
+      signOptions: { expiresIn: '24h' }, // Ajusta el tiempo de expiración según tus necesidades
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
